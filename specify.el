6aa7d1bec6266aa6ff0d43d1af404e38f44b445d
Ben Anhalt <anhalt@ku.edu>
2013-05-02 18:00:25 -0500

    login and out working pretty good

--- specify.el
+++ specify.el
@@ -10,13 +10,17 @@
 (defvar specify/http-header-line
   "^\\([^:]+\\): \\(.+\\)$")
 
+(defun specify/status-success-p (status)
+  (let ((code (car status)))
+    (and (>= code 200) (< code 300))))
+
 (defun specify/parse-status-line ()
   (if (not (looking-at specify/http-status-line))
       (message "bad http-status-line: %s"
                (buffer-substring (point) (line-end-position)))
     (progn
       (forward-line)
-      (list (match-string 1) (match-string 2)))))
+      (list (string-to-number (match-string 1)) (match-string 2)))))
 
 (defun specify/parse-headers (&optional headers)
   (cond ((looking-at "^\r?$") headers)
@@ -34,14 +38,26 @@
 
 (defun specify/get (url)
   (let ((buffer (url-retrieve-synchronously url)))
-    (with-current-buffer buffer
-      (unwind-protect
-          (progn
-            (goto-char (point-min))
-            (list (specify/parse-status-line)
-                  (specify/parse-headers)
-                  (let ((json-object-type 'hash-table)) (json-read))))
-        (kill-buffer buffer)))))
+    (specify/process-response url buffer)))
+
+(defun specify/process-response (url buffer)
+  (with-current-buffer buffer
+    (unwind-protect
+        (progn
+          (goto-char (point-min))
+          (list url
+                (specify/parse-status-line)
+                (specify/parse-headers)
+                (let ((json-object-type 'hash-table))
+                  (condition-case nil (json-read) (end-of-file nil)))))
+      (kill-buffer buffer))))
+
+(defun specify/put (url data)
+  (let* ((url-request-method "PUT")
+         (url-request-extra-headers '(("Content-Type" . "application/json")))
+         (url-request-data (json-encode data))
+         (buffer (url-retrieve-synchronously url)))
+    (specify/process-response url buffer)))
 
 (defvar specify/server-hist nil)
 
@@ -51,8 +67,11 @@
 
 (defvar specify/username-hist nil)
 (defvar specify/collection-hist nil)
+(defvar specify/login-data nil)
 
-(defun specify/login (status headers data)
+(defun specify/login (url status headers data)
+  (setq specify/login-data (copy-hash-table data))
+
   (puthash "username"
            (read-from-minibuffer "Username: "
                                  nil nil nil
@@ -70,8 +89,38 @@
                       collections)
              data))
 
-  (message "login: %s" (json-encode data)))
+  (apply 'specify/complete-login (specify/put url data)))
 
-;;(specify/get "http://127.0.0.1:8000/context/login/")
+
+(defvar specify/api-url nil)
+
+(defun specify/complete-login (url status headers data)
+  (setq specify/api-url
+        (replace-regexp-in-string "/context/login/" "/api/specify/" url)))
+
+(defun specify-logout ()
+  (interactive)
+  (if (not specify/api-url) (error "Not logged in."))
+  (let ((url (replace-regexp-in-string "/api/specify/" "/context/login/" specify/api-url))
+        (data (copy-hash-table specify/login-data)))
+    (puthash "username" nil data)
+    (apply 'specify/logout-complete (specify/put url data))))
+
+(defun specify/logout-complete (url status headers data)
+  (if (not (specify/status-success-p status)) (error "Logout failed."))
+  (setq specify/api-url nil)
+  (message "Logged out."))
+
+(defvar specify/get-uri-hist nil)
+
+(defun specify-get (uri)
+  (interactive (list (read-string "URI: " nil 'specify/get-uri-hist)))
+  (if (not specify/api-url) (error "Not logged in.")
+    (apply 'specify/show-result (specify/get (concat specify/api-url uri)))))
+
+(defun specify/show-result (url status headers data)
+  (with-output-to-temp-buffer url
+      (print status)
+      (print data)))
 
 (provide 'specify)
